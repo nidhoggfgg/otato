05fc4a774dbd5eb6790884a7990a08b3ee44e316
nidhoggfgg <nidhoggfgg@gmail.com>
2023-07-24 23:16:06 +0800

    👌

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,3 @@ import './style.css'
 import '/l2d/main'
 
 // document.querySelector('#live2d').innerHTML = ``
-
-// stupid, but i can do nothing
-(function prependBase() {
-  document.querySelectorAll("a").forEach((link) => {
-    let url = link.getAttribute("href");
-    if (url?.startsWith("/")) {
-      url = import.meta.env.BASE_URL + url.slice(1);
-      link.setAttribute("href", url);
-    }
-  });
-})();
